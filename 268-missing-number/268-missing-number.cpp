d83d1da216f25ac0edda46f595e3b217063204fa
sudeepverma898 <102410001+sudeepverma898@users.noreply.github.com>
2022-04-03 00:27:17 +0530

    Time: 23 ms (68.61%), Space: 18 MB (65.02%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans=0;
+        int s=nums.size();
+        for(int i=0; i<=nums.size(); i++)
+            ans=ans^i;
+        for(int i=0; i<nums.size(); i++)
+            ans^=nums[i];
+        return ans;
+    }
+};
