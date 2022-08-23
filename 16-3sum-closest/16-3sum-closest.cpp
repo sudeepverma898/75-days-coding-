83c02524c07f04ad26d1d1d8e08437bfe3d7a694
sudeepverma898 <102410001+sudeepverma898@users.noreply.github.com>
2022-08-24 03:44:43 +0530

    Time: 264 ms (94.67%), Space: 16.3 MB (75.57%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -6,27 +6,19 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0; i<=nums.size()-3; i++){
             int left=i+1,right=nums.size()-1;
-            sum=nums[left]+nums[right]+nums[i];
-            if(abs(target-sum)<min)
-            {
-                min=abs(target-sum);
-                minf=nums[left]; mins=nums[right]; mint=nums[i];
-            }
             while(left<right){
-                if(sum==target)
-                    return sum;
-                else if(sum<target){
-                    left++;
-                }
-                else{
-                    right--;
-                }
                 sum=nums[left]+nums[right]+nums[i];
-                if(right!=left && abs(target-sum)<min)
-                { 
+                if(abs(target-sum)<min)
+                {
                     min=abs(target-sum);
                     minf=nums[left]; mins=nums[right]; mint=nums[i];
                 }
+                if(sum==target)
+                    return sum;
+                else if(sum<target)
+                    left++;
+                else
+                    right--;
             }
         }
         return minf+mins+mint;
